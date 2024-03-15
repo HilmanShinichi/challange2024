70307ba16b1c97fc4cec60167b5bf354faaed327
Hilman Septiana <hilmanseptiana19@gmail.com>
2024-03-15 11:10:44 +0700

    add day 25

---
+++ 25.DoubleChar.js
@@ -0,0 +1,20 @@
+// https://www.codewars.com/kata/56b1f01c247c01db92000076/solutions/javascript
+
+function doubleChar(str) {
+  let result = '';
+
+  for (let i = 0; i < str.length; i++) {
+    result += str[i]
+    result += str[i]
+  }
+
+  return result;
+}
+
+function doubleChar(str) {
+
+ return str.split('').map((e) => e + e).join('') 
+}
+
+console.log(doubleChar("abcd")); // "aabbccdd"
+console.log(doubleChar("Adidas")); // "AAddiiddaass"
