bbbf6533c6cb69cd190a4e2363439ceb72d2fe65
Hilman Septiana <hilmanseptiana19@gmail.com>
2024-03-30 21:29:16 +0700

    diff

--- BeginnerSeriesCockroach.js
+++
@@ -1,9 +0,0 @@
-// https://www.codewars.com/kata/55fab1ffda3e2e44f00000c6/train/javascript
-
-function cockroachSpeed(s) {
-  return Math.floor((s * 100000) / 3600);
-}
-
-console.log(cockroachSpeed(1.08)); // 30
-console.log(cockroachSpeed(1.09)); // 30
-console.log(cockroachSpeed(0)); // 0
