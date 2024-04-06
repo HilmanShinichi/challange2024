c39de87ef925ca63f121e9ff55a9bcae598c24ec
Hilman Septiana <hilmanseptiana19@gmail.com>
2024-04-06 13:07:58 +0700

    add no day

---
+++ SumTheStrings.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/5966e33c4e686b508700002d/solutions/javascript
+
+function sumStr(a,b) {
+    return String(Number(a)+Number(b));
+}
+
+console.log(sumStr("4","5")); // '9'
+console.log(sumStr("34","5")); // '39'
