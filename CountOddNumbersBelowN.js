502f9a0da6467b971c6c8c45c6551b3682ec2db8
Hilman Septiana <hilmanseptiana19@gmail.com>
2024-04-06 13:59:53 +0700

    add no day

---
+++ CountOddNumbersBelowN.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/59342039eb450e39970000a6/solutions/javascript
+
+const oddCount = n => Math.floor(n/2) ;
+
+console.log(oddCount(7)); // 3
+console.log(oddCount(15)); // 3
+console.log(oddCount(15023)); // 3
